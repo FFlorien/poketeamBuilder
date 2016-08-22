f2e905780dc5ada217583065ba6a102d4fd20024
Florien Flament <florien@movify.be>
2016-08-22 17:43:14 +0200

    Not there yet for gradle conf

---
+++ app/src/main/java/be/florien/poketeam/MyTestClass.java
@@ -0,0 +1,13 @@
+package be.florien.poketeam;
+
+import be.florien.joinorm.annotation.JoTable
+
+/**
+ * Created by FlamentF on 22-08-16.
+ */
+
+@JoTable
+public class MyTestClass {
+
+    private String tutu;
+}
